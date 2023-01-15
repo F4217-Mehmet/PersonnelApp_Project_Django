e7613419346fae136457dbabf6c95548e98399e7
Mehmet Kabasakal <sehriistanbul@gmail.com>
2023-01-16 01:28:01 +0300

    permission updated

--- personnel/views.py
+++ personnel/views.py
@@ -3,6 +3,8 @@ from .serializers import DepartmentSerializer, PersonnelSerializer
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsStafforReadOnly
+from rest_framework import generics, status
+from rest_framework.response import Response
 
 
 class DepartmentView(generics.ListCreateAPIView):
@@ -14,6 +16,36 @@ class PersonnelListCreateView(generics.ListCreateAPIView):
     serializer_class = PersonnelSerializer
     queryset = Personnel.objects.all()
     permission_classes = [IsAuthenticated, IsStafforReadOnly]
+
+class PersonnelListCreateView(generics.ListCreateAPIView):
+    serializer_class = PersonnelSerializer
+    queryset = Personnel.objects.all()
+    # permission_classes = [IsAuthenticated, IsStafforReadOnly]
+    permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        if self.request.user.is_staff:
+            personnel = self.perform_create(serializer)
+            data = {
+                "message": f"Personnel {personnel.first_name} saved successfully",
+                "personnel": serializer.data
+            }
+        else:
+            data = {
+                "message": "You're not authorized to perform this operation"
+            }
+            return Response(data, status=status.HTTP_401_UNAUTHORIZED)
+            
+        headers = self.get_success_headers(serializer.data)
+        return Response(data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        person = serializer.save()
+        person.create_user = self.request.user
+        person.save()
+        return person
     
 
 
